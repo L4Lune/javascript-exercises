3519448a6e084a7b4d07d005dded6c0f568e87b2
Jack Roudebush <roudebushjack@gmail.com>
2023-03-19 22:06:56 -0400

    Complete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = string.split('');
+    console.log(stringArray);
 
+    let reversedArray = stringArray.reverse();
+    console.log(reversedArray);
+
+    let reversedString = reversedArray.join("");
+    console.log(reversedString);
+
+    return reversedString;
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
