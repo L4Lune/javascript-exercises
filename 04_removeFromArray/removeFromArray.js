84c5658638b4374a956d4eecea3b28f1419a8cd1
Jack Roudebush <roudebushjack@gmail.com>
2023-03-20 23:47:36 -0400

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    const modifiedArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            modifiedArray.push(item);
+        }
+    })
+    return modifiedArray;
 };
 
 // Do not edit below this line
