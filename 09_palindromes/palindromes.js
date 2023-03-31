bf470ac7df61ecbd336edb559f0c7a9447105149
Jack Roudebush <roudebushjack@gmail.com>
2023-03-31 15:36:47 -0400

    I think I got it on the first go

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const replaced = string.replace(/[^a-z0-9]/gi, '');
+    console.log(replaced);
+    let replacedLower = replaced.toLowerCase();
+    console.log(replacedLower);
 
+    let stringArray = replacedLower.split('');
+    console.log(stringArray);
+   
+    let reversedArray = stringArray.reverse();
+    console.log(reversedArray);
+
+    let reversedString = reversedArray.join("");
+    console.log(reversedString);
+
+    if (replacedLower === reversedString) {
+        return true
+    } else return false;
 };
 
 // Do not edit below this line
