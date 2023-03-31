65efec18c8b374c6d8b99e3b1fb89c436ff7255b
Jack Roudebush <roudebushjack@gmail.com>
2023-03-31 16:02:25 -0400

    Made Binets formula in js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,6 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    fib = ((1 / (Math.sqrt(5)))) * (((1 + Math.sqrt(5)) / 2))**n - (1 - ((Math.sqrt(5)) / 2))**n
+    return fib;
 };
 
 // Do not edit below this line
